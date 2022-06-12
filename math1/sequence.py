5dbdd6a24807192c21671a591a0508fb557eeafc
jcy1511 <jcy1511@gmail.com>
2022-06-12 21:38:18 +0900

    99% completed

--- math1/sequence.py
+++ math1/sequence.py
@@ -1,10 +1,16 @@
+import sympy as sp
+
+
 class Sequence:
 
-    def __init__(self, seq):
-        self.seq = seq
+    def __init__(self):
         self.type = "null"
-        self.cd = "공차를 구할 수 없습니다"
-        self.cr = "공비를 구할 수 없습니다"
+        self.an = "null"
+        self.cd = "null"
+        self.cr = "null"
+
+    def setDataBySeq(self, seq):
+        self.seq = seq
 
         for i in range(len(seq)-2):
             if self.seq[i+1]*2 == self.seq[i]+self.seq[i+2]:
@@ -23,23 +29,67 @@ class Sequence:
                     break
 
         if self.type == "등차수열":
-            self.cd = self.seq[1]-self.seq[0]
+            self.an = sp.expand(sp.sympify(
+                f"{self.seq[0]}+(n-1)*{self.seq[1]-self.seq[0]}"))
+            self.getCdOrCr()
         elif self.type == "등비수열":
-            self.cr = self.seq[1]/self.seq[0]
-
-    def a(self, n):
-        if self.type == "등차수열":
-            return self.seq[0] + (n-1)*(self.seq[1]-self.seq[0])
-
-        elif self.type == "등비수열":
-            return self.seq[0] * (self.seq[1]/self.seq[0])**(n-1)
-
+            self.an = sp.expand(sp.sympify(
+                f"{self.seq[0]}*{self.seq[1]/self.seq[0]}**(n-1)"))
+            self.getCdOrCr()
         else:
-            return "수열의 일반항을 정의할 수 없습니다"
+            print("수열의 일반항을 정의할 수 없습니다.")
 
-    def s(self, n):
+    def getCdOrCr(self):
+        n = sp.Symbol('n')
+
+        if self.an.subs(n, 1)*2 == self.an.subs(n, 0)+self.an.subs(n, 2):
+            self.type = "등차수열"
+            self.cd = float(self.an.subs(n, 1)-self.an.subs(n, 0))
+
+        elif self.an.subs(n, 1)**2 == self.an.subs(n, 0)*self.an.subs(n, 2):
+            self.type = "등비수열"
+            self.cr = float(self.an.subs(n, 1)/self.an.subs(n, 0))
+
+    def setDataByAn(self, an):
+        self.an = sp.sympify(an)
+        self.getCdOrCr()
+
+    def setDataBySn(self, sn):
+        n = sp.Symbol('n')
+        Sn = sp.sympify(sn)
+        Sn1 = Sn.subs(n, n-1)
+        an = sp.expand(Sn - Sn1)
+
+        if an.subs(n, 1) == Sn.subs(n, 1):
+            self.an = an
+        else:
+            print(f"a1 = {an.subs(n, 1)}, an = {an} (a>=2)")
+            self.an = an
+        self.getCdOrCr()
+
+    def a(self, nn):
+        n = sp.Symbol('n')
+        return float(self.an.subs(n, nn))
+
+    def s(self, nn):
+        n = sp.Symbol('n')
         sum = 0
-        for i in range(n):
-            sum += self.seq[i]
+        for i in range(nn):
+            sum += self.an.subs(n, i+1)
 
-        return sum
+        return float(sum)
+
+    def info(self):
+        print("an =", self.an)
+        if self.type == "등차수열":
+            print("종류 : 등차수열")
+            print("공차 =", self.cd)
+        elif self.type == "등비수열":
+            print("종류 : 등비수열")
+            print("공비 =", self.cr)
+
+
+a = Sequence()
+a.setDataBySeq([1, 3, 5])
+print(a.a(2))
+a.info()
--- math2/test/test.py
+++ math2/test/test.py
@@ -1,4 +1,16 @@
-z = 1
-for i in range(100):
-    print(z)
-    z += 0.01
+from __future__ import print_function
+import sympy as sp
+
+
+def setDataBySn(sn):
+    n = sp.Symbol('n')
+    Sn = sp.sympify(sn)
+    Sn1 = Sn.subs(n, n-1)
+    an = sp.expand(Sn - Sn1)
+
+    if an.subs(n, 1) == Sn.subs(n, 1):
+        return an
+    else:
+
+
+print(setDataBySn("n**2+2*n"))
